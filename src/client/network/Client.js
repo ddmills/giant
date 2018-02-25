fa30de05746c03036e444efacc2097304f205325
ddmills <mills.dalton@gmail.com>
2018-02-24 23:03:58 -0600

    only one timeout

--- src/client/network/Client.js
+++ src/client/network/Client.js
@@ -2,6 +2,8 @@ import Store from '../store/Store';
 import {SERVER_CONNECTED, SERVER_DISCONNECTED, SERVER_LATENCY} from '../store/actions/ActionTypes';
 
 export function listen(socket) {
+  let latencyTimeout;
+
   const getLatency = () => {
     const start = Date.now();
 
@@ -10,13 +12,21 @@ export function listen(socket) {
         type: SERVER_LATENCY,
         latency: Date.now() - start
       });
-      setTimeout(getLatency, 3000);
+      latencyTimeout = setTimeout(getLatency, 2000);
     });
   };
 
-  socket.on('disconnect', () => Store.dispatch({type: SERVER_DISCONNECTED}));
+  socket.on('disconnect', () => {
+    clearTimeout(latencyTimeout);
+    Store.dispatch({
+      type: SERVER_DISCONNECTED
+    });
+  });
+
   socket.on('connect', () => {
     getLatency();
-    Store.dispatch({type: SERVER_CONNECTED});
+    Store.dispatch({
+      type: SERVER_CONNECTED
+    });
   });
 }
